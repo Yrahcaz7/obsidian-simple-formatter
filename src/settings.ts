f935241e19a249af427c8c5624ace2eee513b8d8
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2026-07-26 17:43:37 -0400

    use setting groups & update manifest

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
-import { App, PluginSettingTab, Setting } from 'obsidian';
+import { App, PluginSettingTab, SettingGroup } from 'obsidian';
 import SimpleFormatterPlugin from './main';
 
 export interface SimpleFormatterPluginSettings {
@@ -24,66 +24,65 @@ export class SimpleFormatterSettingTab extends PluginSettingTab {
 	}
 
 	display() {
-		const { containerEl } = this;
+		this.containerEl.empty();
 
-		containerEl.empty();
+		const generalGroup = new SettingGroup(this.containerEl);
 
-		new Setting(containerEl)
+		generalGroup.addSetting(setting => setting
 			.setName('HTML mode')
 			.setDesc('When enabled, HTML paragraphs are used for alignment and indentation. This has greater cross-compatability than the default, but it disallows Markdown syntax inside aligned and indented blocks. It also changes how such blocks are displayed in editing mode. (This only changes new placements, not old ones.)')
-			.addToggle(toggle =>
-				toggle
-					.setValue(this.plugin.settings.htmlMode)
-					.onChange(async value => {
-						this.plugin.settings.htmlMode = value;
-						await this.plugin.saveSettings();
-					}),
-			);
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.htmlMode)
+				.onChange(async value => {
+					this.plugin.settings.htmlMode = value;
+					await this.plugin.saveSettings();
+				}),
+			)
+		);
 
-		new Setting(containerEl).setName('Indentation').setHeading();
+		const indentationGroup = new SettingGroup(this.containerEl).setHeading('Indentation');
 
-		new Setting(containerEl)
+		indentationGroup.addSetting(setting => setting
 			.setName('Indentation amount')
 			.setDesc('The amount of indentation used by the "Indent/Unindent line(s)" commands.')
-			.addSlider(slider =>
-				slider
-					.setLimits(1, 4, 0.5)
-					.setDynamicTooltip()
-					.setValue(this.plugin.settings.indentAmount)
-					.onChange(async value => {
-						this.plugin.settings.indentAmount = value;
-						await this.plugin.saveSettings();
-					}),
-			);
+			.addSlider(slider => slider
+				.setLimits(1, 4, 0.5)
+				.setDynamicTooltip()
+				.setValue(this.plugin.settings.indentAmount)
+				.onChange(async value => {
+					this.plugin.settings.indentAmount = value;
+					await this.plugin.saveSettings();
+				}),
+			)
+		);
 
-		new Setting(containerEl).setName('Section breaks').setHeading();
+		const sectionBreaksGroup = new SettingGroup(this.containerEl).setHeading('Section breaks');
 
-		new Setting(containerEl)
+		sectionBreaksGroup.addSetting(setting => setting
 			.setName('Section break indicator')
 			.setDesc('The characters inserted by the "Insert section break" command. Defaults to "⁂".')
-			.addText(text =>
-				text
-					.setPlaceholder('Enter indicator here')
-					.setValue(this.plugin.settings.sectionBreak)
-					.onChange(async value => {
-						this.plugin.settings.sectionBreak = value;
-						await this.plugin.saveSettings();
-					}),
-			);
-
-		new Setting(containerEl)
+			.addText(text => text
+				.setPlaceholder('Enter indicator here')
+				.setValue(this.plugin.settings.sectionBreak)
+				.onChange(async value => {
+					this.plugin.settings.sectionBreak = value;
+					await this.plugin.saveSettings();
+				}),
+			)
+		);
+		sectionBreaksGroup.addSetting(setting => setting
 			.setName('Section break alignment')
 			.setDesc('The alignment of the characters inserted by the "Insert section break" command.')
-			.addDropdown(dropdown =>
-				dropdown
-					.addOption('left', 'Left')
-					.addOption('center', 'Center')
-					.addOption('right', 'Right')
-					.setValue(this.plugin.settings.sectionBreakAlign)
-					.onChange(async value => {
-						this.plugin.settings.sectionBreakAlign = value;
-						await this.plugin.saveSettings();
-					}),
-			);
+			.addDropdown(dropdown => dropdown
+				.addOption('left', 'Left')
+				.addOption('center', 'Center')
+				.addOption('right', 'Right')
+				.setValue(this.plugin.settings.sectionBreakAlign)
+				.onChange(async value => {
+					this.plugin.settings.sectionBreakAlign = value;
+					await this.plugin.saveSettings();
+				}),
+			)
+		);
 	}
 }
