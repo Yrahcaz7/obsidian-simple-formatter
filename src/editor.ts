78ddf6019c8f49806f1a1a1fd138e55c5bf3c7ca
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2026-07-27 14:23:10 -0400

    allow formatted multi-line paragraphs

--- src/editor.ts
+++ src/editor.ts
@@ -8,6 +8,8 @@ import {
 	ViewUpdate,
 } from '@codemirror/view';
 
+const PLAIN_LINE_CLASS_NAMES = new Set<string>(['cm-active cm-line', 'cm-line']);
+
 class SimpleFormatPlugin implements PluginValue {
 	decorations: DecorationSet;
 
@@ -29,55 +31,51 @@ class SimpleFormatPlugin implements PluginValue {
 		const visitedLines = new Set<number>();
 
 		for (const range of view.visibleRanges) {
-			let prevLine: Line | undefined;
-			const lastLine = view.state.doc.lineAt(range.to);
-			while (true) {
+			const fromLineNum = view.state.doc.lineAt(range.from).number;
+			const toLineNum = view.state.doc.lineAt(range.to).number;
+			let paragraphLines: Line[] = [];
+			for (let lineNum = fromLineNum; lineNum <= toLineNum; lineNum++) {
 				// Get next line
-				let line: Line;
-				if (prevLine === undefined) {
-					line = view.state.doc.lineAt(range.from);
-				} else if (prevLine.number > lastLine.number || prevLine.number >= view.state.doc.lines) {
-					break;
+				const line = view.state.doc.line(lineNum);
+				let lineElement: HTMLElement | null = null;
+				// Update paragraph
+				if (line.text === '') {
+					paragraphLines = [];
 				} else {
-					line = view.state.doc.line(prevLine.number + 1);
+					lineElement = this.getLineElement(view, line);
+					if (!lineElement || !PLAIN_LINE_CLASS_NAMES.has(lineElement.className)) {
+						paragraphLines = [];
+					}
+					paragraphLines.push(line);
 				}
-				prevLine = line;
 				// Skip already visited lines
-				if (visitedLines.has(line.number)) {
+				if (visitedLines.has(lineNum)) {
 					continue;
 				}
-				visitedLines.add(line.number);
+				visitedLines.add(lineNum);
+				// Skip codeblocks
+				if (lineElement?.classList.contains('HyperMD-codeblock')) {
+					continue;
+				}
 				// Parse format syntax
 				if (!line.text.endsWith('}')) continue;
 				const matches = line.text.match(/\s*\{\s*style="([^"]+)"\s*\}$/u);
 				if (!matches) continue;
-				// Skip codeblocks
-				try {
-					if ([line.from, line.to].some(position => {
-						const parentClasses = view?.domAtPos(position)?.node?.parentElement?.classList;
-						return parentClasses?.contains('cm-hmd-codeblock');
-					})) {
-						continue;
-					}
-				} catch (error) {
-					if (error instanceof TypeError) {
-						// continue on if parent element does not exist
-					} else {
-						throw error;
-					}
-				}
 				// Apply formatting
 				let styles = matches[1];
 				if (styles) {
 					styles = styles.replace(/;/g, ' !important;');
 					if (!styles.match(/;\s*$/)) styles += ' !important';
-					builder.add(
-						line.from,
-						line.from,
-						Decoration.line({
-							attributes: {style: styles},
-						}),
-					);
+					for (const paragraphLine of paragraphLines) {
+						builder.add(
+							paragraphLine.from,
+							paragraphLine.from,
+							Decoration.line({
+								attributes: {style: styles},
+							}),
+						);
+					}
+					paragraphLines = [];
 				}
 				// Hide format syntax if in live preview mode and the line is not selected
 				if (livePreview && !view.state.selection.ranges.some(selectedRange => (selectedRange.from <= line.to) && (selectedRange.to >= line.from))) {
@@ -91,6 +89,20 @@ class SimpleFormatPlugin implements PluginValue {
 			}
 		}
 		return builder.finish();
+	}
+
+	private getLineElement(view: EditorView, line: Line): HTMLElement | null {
+		try {
+			const node = view.domAtPos(line.from)?.node;
+			let element = (node instanceof HTMLElement ? node : node.parentElement);
+			while (element !== null) {
+				if (element.classList.contains('cm-line')) return element;
+				element = element.parentElement;
+			}
+			return null;
+		} catch {
+			return null;
+		}
 	}
 }
 
